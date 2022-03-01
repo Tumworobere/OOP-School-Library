675d1124875729a59147d027bc0658cb8eff6aa5
Annah Tomoro <annaht.annah@gmail.com>
2022-03-01 16:57:02 +0300

    Fix errors

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.ran(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -14,6 +14,6 @@ class Person
   end
 
   def can_use_services?
-    of_age? || parent_permission == true
+    of_age? || @parent_permission
   end
 end
