bf9879212c8971689135d5d0b0919317aefd0e71
Annah Tomoro <annaht.annah@gmail.com>
2022-03-01 17:08:25 +0300

    implement requested changes

--- person.rb
+++ person.rb
@@ -1,19 +1,24 @@
 class Person
-  attr_accessor :name, :age
-  attr_reader :id
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
+    attr_accessor :name, :age
+    attr_reader :id
+  
+    def initialize(age, name = 'Unknown', parent_permission: true)
+      @id = Random.rand(1..10.000)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  
+    private
+  
+    def of_age?
+      @age >= 18
+    end
+  
+    public
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
   end
-  private :of_age?
-  def of_age?
-    @age >= 18
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-end
+  
