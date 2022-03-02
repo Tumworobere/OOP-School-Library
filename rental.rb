c00a51dbf6b4d17d92655feff6984f425c222723
Annah Tomoro <annaht.annah@gmail.com>
2022-03-02 08:41:37 +0300

    Fix linter errors

--- rental.rb
+++ rental.rb
@@ -1,13 +1,12 @@
 class Rental
-    attr_accessor :date
-    attr_reader :book, :person
-  
-    def initialize(date, book, person)
-      @date = date
-      @person = person
-      person.rentals << self
-      @book = book
-      book.rentals << self
-    end
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+    @person = person
+    person.rentals << self
+    @book = book
+    book.rentals << self
   end
-  
+end
