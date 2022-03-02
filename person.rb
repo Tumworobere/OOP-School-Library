3894b7176efc4cf2d5b9b943899ed1bb070a5b91
Annah Tomoro <annaht.annah@gmail.com>
2022-03-02 17:59:30 +0300

    Update classes

--- person.rb
+++ person.rb
@@ -1,16 +1,28 @@
 require_relative 'corrector'
-require_relative 'rental'
 
 class Person
   attr_accessor :name, :age, :parent_permission, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..10.000)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def add_rentals(person)
+    @rentals.push(person)
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
@@ -19,18 +31,7 @@ class Person
     @age >= 18
   end
 
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  def validate_name
-    @name = @corrector.correct_name(@name)
+  def to_s
+    "Name: #{@name}, ID: #{@id}, Age: #{@age}"
   end
 end
-
-person = Person.new(110, 'annah-tumworobere')
-person.validate_name
-
-puts person.name
