dbc3df3ecab16e230488edea7b036cfa3114dd18
Annah Tomoro <annaht.annah@gmail.com>
2022-03-02 09:23:57 +0300

    Added files for Basic UI

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
+require_relative 'person'
+require_relative 'student'
+require_relative 'rental'
+require_relative 'book'
+require_relative 'teacher'
+require_relative 'classroom'
+
 class App
     def initialize
       @books = []
